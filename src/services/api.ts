d81a3fa216d24ce03b31d87f758644456970ab79
victorgpd <victorguilherme2407@outlook.com>
2025-09-01 20:58:30 -0300

    versão 1.1

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { auth } from "./firebase";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://unified-muskrat-known.ngrok-free.app/api/viva-store",
   headers: {
     "Content-Type": "application/json",
   },
